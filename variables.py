496d2a138ddbae8fa7e840cf47cd6d184d7ae188
Paolo Soncco <paholo2054@gmail.com>
2023-09-18 22:48:18 -0500

    first commit

---
+++ variables.py
@@ -0,0 +1,23 @@
+# Tipos de variables
+
+numero = 10
+string = "Hola mundo"
+booleano = True
+flotante = 10.5
+lista = [1, 2, 3, 4, 5]
+tupla = (1, 2, 3, 4, 5)
+diccionario = {
+    "nombre": "Juan",
+    "apellido": "Perez"
+}
+
+a , b, c = 1, 2, 3
+
+# Nombrar variables
+
+minumero = 20
+mi_numero = 20
+miNumero = 20
+_mi_numero = 20
+MINUMERO = 20
+miNumero20 = 20
