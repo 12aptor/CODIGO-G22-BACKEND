ca315e947adf769caf96eaabc7b61c2a87e9e46c
César Mayta <cesarmayta@gmail.com>
2023-09-25 22:05:46 -0500

    dia1 ofertas laborales

---
+++ dia1/ofertaslaborales/app.py
@@ -0,0 +1,22 @@
+from flask import Flask,request,render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    titulo = "OFERTAS LABORALES"
+    lista_ofertas = [
+        {
+            "titulo":"FRONTEND DEVELOPER REACT",
+            "imagen":"https://sigdeletras.com/images/blog/202004_react_leaflet/react.png",
+            "descripcion":"Buscamos frontend developer con experiencia en react"
+        },
+        {
+            "titulo":"BACKEN DEVELOPER PYTHON",
+            "imagen":"https://beapythondev.files.wordpress.com/2019/06/python.png?w=365&h=365&crop=1",
+            "descripcion":"Buscamos backend developer con python , buena paga"
+        }
+    ]
+    return render_template('index.html',titulo=titulo)
+
+app.run(debug=True)
