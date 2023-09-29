19b257b4676097424ed7a7784bbcc2a17d587ed3
César Mayta <cesarmayta@gmail.com>
2023-09-28 20:44:44 -0500

    dia4 blueprint admin

--- dia4/app/__init__.py
+++ dia4/app/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 
 from .portafolio import portafolio
+from .admin import admin
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(portafolio)
+    app.register_blueprint(admin)
     
     return app
---
+++ dia4/app/admin/views.py
@@ -0,0 +1,6 @@
+from flask import render_template,request
+from . import admin
+
+@admin.route('/')
+def index():
+    return render_template('admin/index.html')
