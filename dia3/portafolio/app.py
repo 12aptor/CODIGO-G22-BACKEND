d63fb080dab06b36de545d3f2f0d46c8b2e4b3dd
César Mayta <cesarmayta@gmail.com>
2023-09-28 19:40:33 -0500

    dia3 sesiones simple

--- dia3/portafolio/app.py
+++ dia3/portafolio/app.py
@@ -1,9 +1,10 @@
 import json
 import requests
-from flask import Flask,request,render_template
+from flask import Flask,request,render_template,session
 from firebase import FirebaseAdmin
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "qwerty123"
 
 fb = FirebaseAdmin()
 
@@ -19,6 +20,8 @@ def index():
     data = requests.get(URL_GITHUB).json()
     print(data)
     context = fb.get_document('perfil','MKuNXNh1wdxyWZUV5MkH')
+    session['nombre'] = context['nombre']
+    session['curriculum'] = context['curriculum']
     return render_template('index.html',**context)
 
 @app.route('/cv')
