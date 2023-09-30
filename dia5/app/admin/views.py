9bf64f94e6fb9f39723cd090ec0066b2e2a61f47
César Mayta <cesarmayta@gmail.com>
2023-09-29 21:52:12 -0500

    wtf

--- dia5/app/admin/views.py
+++ dia5/app/admin/views.py
@@ -3,6 +3,8 @@ from . import admin
 
 from app import fb
 
+from .forms import ExperienciaForm
+
 @admin.route('/')
 def index():
     return render_template('admin/index.html')
@@ -20,8 +22,11 @@ def experiencia():
     READ EXPERIENCIAS
     """
     data = fb.get_collection('experiencia')
+    experiencia_form = ExperienciaForm()
+    
     context = {
-        'experiencias':data
+        'experiencias':data,
+        'form':experiencia_form
     }
     print(context)
     return render_template('admin/experiencia.html',**context)
