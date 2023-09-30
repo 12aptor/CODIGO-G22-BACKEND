e7f2754d46b626440d070bf8e1f6b457a5b15b26
César Mayta <cesarmayta@gmail.com>
2023-09-30 09:35:39 -0500

    dia6

---
+++ dia6/app/__init__.py
@@ -0,0 +1,22 @@
+from flask import Flask
+from app.firebase import FirebaseAdmin
+from flask_bootstrap import Bootstrap4
+
+fb = FirebaseAdmin()
+
+
+#blueprints
+from .portafolio import portafolio
+from .admin import admin
+
+from .config import Config
+
+def create_app():
+    app = Flask(__name__)
+    bootstrap = Bootstrap4(app)
+    
+    app.config.from_object(Config)
+    app.register_blueprint(portafolio)
+    app.register_blueprint(admin)
+    
+    return app
