d3a9b46dad6e8554f4b14e829a842e40eaa2fcf3
César Mayta <cesarmayta@gmail.com>
2023-09-29 20:54:41 -0500

    dia5 read experiencias

--- dia5/app/admin/views.py
+++ dia5/app/admin/views.py
@@ -1,10 +1,27 @@
 from flask import render_template,request
 from . import admin
 
+from app import fb
+
 @admin.route('/')
 def index():
     return render_template('admin/index.html')
 
+""" 
+CRUD DE EXPERIENCIAS
+C - CREATE
+R - READ
+U - UPDATE
+D - DELETE
+"""
 @admin.route('/experiencia')
 def experiencia():
-    return render_template('admin/experiencia.html')
+    """
+    READ EXPERIENCIAS
+    """
+    data = fb.get_collection('experiencia')
+    context = {
+        'experiencias':data
+    }
+    print(context)
+    return render_template('admin/experiencia.html',**context)
