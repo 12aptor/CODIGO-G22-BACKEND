efaa219960e55f7121cf06dbeca451f91611c465
César Mayta <cesarmayta@gmail.com>
2023-10-03 22:53:08 -0500

    dia2 joins

--- dia2/joins.sql
+++ dia2/joins.sql
@@ -17,3 +17,18 @@ group by alumno.nombres,alumno.apellidos
 -- promedio de nota por curso
 -- python 20
 -- flask 11
+select curso,avg(nota)
+from nota
+group by curso
+
+select alumno.nombres,alumno.apellidos,
+nota1.nota as 'PYTHON',
+nota2.nota as 'FLASK',
+nota3.nota as 'MYSQL'
+from alumno
+left join nota nota1 on nota1.alumno_id = alumno.id and nota1.curso = 'PYTHON'
+left join nota nota2 on nota2.alumno_id = alumno.id and nota2.curso = 'FLASK'
+left join nota nota3 on nota3.alumno_id = alumno.id and nota3.curso = 'MYSQL'
+
+
+
