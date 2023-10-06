3debf09670c01da811e9432efd3b248c9b5ba769
César Mayta <cesarmayta@gmail.com>
2023-10-05 21:58:03 -0500

    dia4 cursos html

--- dia4/asistencia/app.py
+++ dia4/asistencia/app.py
@@ -8,12 +8,22 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'db_asistencia'
+app.config["MYSQL_CURSORCLASS"] = "DictCursor"
 
 mysql = MySQL(app)
 print("conectado a base de datos")
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    cursor = mysql.connection.cursor()
+    sqlCursos = "select curso_id,curso_nombre from tbl_curso"
+    cursor.execute(sqlCursos)
+    data_cursos = cursor.fetchall()
+    cursor.close()
+    print(data_cursos)
+    context = {
+        'cursos':data_cursos
+    }
+    return render_template('index.html',**context)
 
 app.run(debug=True)
