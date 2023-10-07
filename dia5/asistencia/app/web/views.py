690a96a65a1a6c34e09bb1a8b1d4b09b828262b2
César Mayta <cesarmayta@gmail.com>
2023-10-06 21:38:05 -0500

    dia5 bootstrap

--- dia5/asistencia/app/web/views.py
+++ dia5/asistencia/app/web/views.py
@@ -1,7 +1,21 @@
 from flask import Flask,render_template,request,redirect
 
 from . import web
+from app import db
 
 @web.route('/')
 def index():
     return render_template('index.html')
+
+@web.route('/cursos')
+def cursos():
+    cursor = db.cursor(dictionary=True)
+    sql_cursos = "select curso_id as id,curso_nombre as nombre from tbl_curso"
+    cursor.execute(sql_cursos)
+    data_cursos = cursor.fetchall()
+    cursor.close()
+    print(data_cursos)
+    context = {
+        'data':data_cursos
+    }
+    return render_template('cursos.html',**context)
