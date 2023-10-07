e4396d3368a4807adc17eb1d1ebd928d354e06d5
César Mayta <cesarmayta@gmail.com>
2023-10-06 22:25:45 -0500

    editar cursos

--- dia5/asistencia/app/web/views.py
+++ dia5/asistencia/app/web/views.py
@@ -5,6 +5,14 @@ from app import db
 
 from .forms import CursoForm
 
+def mostrar_cursos():
+    cursor = db.cursor(dictionary=True)
+    sql_cursos = "select curso_id as id,curso_nombre as nombre from tbl_curso"
+    cursor.execute(sql_cursos)
+    data_cursos = cursor.fetchall()
+    cursor.close()
+    return data_cursos
+
 @web.route('/')
 def index():
     return render_template('index.html')
@@ -21,16 +29,29 @@ def cursos():
         db.commit()
         cursor_insert.close()
         return redirect('/cursos')
+
     
-    
-    cursor = db.cursor(dictionary=True)
-    sql_cursos = "select curso_id as id,curso_nombre as nombre from tbl_curso"
-    cursor.execute(sql_cursos)
-    data_cursos = cursor.fetchall()
-    cursor.close()
-    print(data_cursos)
     context = {
-        'cursos':data_cursos,
+        'cursos':mostrar_cursos(),
         'form':form
     }
+    return render_template('cursos.html',**context)
+
+@web.route('/cursos/<id>',methods=['GET','POST'])
+def cursos_editar(id):
+    cursor_editar = db.cursor(dictionary=True)
+    sql_editar = "select curso_nombre as nombre from tbl_curso where curso_id = '"+ id +"'"
+    
+    cursor_editar.execute(sql_editar)
+    data = cursor_editar.fetchall()
+    print(data)
+    cursor_editar.close()
+    
+    form = CursoForm(data=data[0])
+    
+    context = {
+        'cursos':mostrar_cursos(),
+        'form':form
+    }
+    
     return render_template('cursos.html',**context)
