2356fbe65dba34c054b85574bab8e9cd16bb0b9b
César Mayta <cesarmayta@gmail.com>
2023-10-09 20:19:14 -0500

    dia1 get tareas

--- dia1/api/run.py
+++ dia1/api/run.py
@@ -1,6 +1,15 @@
 from flask import Flask,jsonify,request
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
+app.config['MYSQL_DB'] = 'db_api'
+app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
@@ -10,6 +19,15 @@ def index():
     }
     
     return jsonify(context)
+
+@app.route('/tarea')
+def get_tarea():
+    cursor = mysql.connection.cursor()
+    cursor.execute("select text,completed from tbl_tarea")
+    data = cursor.fetchall()
+    cursor.close()
+    
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
