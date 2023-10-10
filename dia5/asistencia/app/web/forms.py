43f3abcfe82564920dfaaf5927d73ee3ce2ecc2d
César Mayta <cesarmayta@gmail.com>
2023-10-09 19:37:59 -0500

    dia1

--- dia5/asistencia/app/web/forms.py
+++
@@ -1,8 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired
-from wtforms.fields import StringField,SubmitField
-
-
-class CursoForm(FlaskForm):
-    nombre = StringField('Nombre',validators=[DataRequired()])
-    submit = SubmitField('Guardar')
