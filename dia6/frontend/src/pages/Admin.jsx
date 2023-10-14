7d2be91377335607d23e37baac4382929befb78f
César Mayta <cesarmayta@gmail.com>
2023-10-14 10:50:16 -0500

    dia6 frontend home listo

--- dia6/frontend/src/pages/Admin.jsx
+++ dia6/frontend/src/pages/Admin.jsx
@@ -1,6 +1,18 @@
+import Footer from "../components/Footer"
+import NavBar from "../components/NavBar"
 function Admin(){
     return(
-        <h1>ADMIN</h1>
+        <>
+        <NavBar/>
+        <section className="py-5">
+            <div className="container px-4 px-lg-5 mt-5">
+                <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+                    <h1>PANEL DE ADMINISTRACIÓN</h1>
+                </div>
+            </div>
+        </section>
+        <Footer/>
+        </>
     )
 }
 
