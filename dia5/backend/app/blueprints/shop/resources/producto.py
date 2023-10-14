a35ac8b620a994991fee86b4e6df599f938febfe
César Mayta <cesarmayta@gmail.com>
2023-10-13 22:52:49 -0500

    dia5 put delete productos

--- dia5/backend/app/blueprints/shop/resources/producto.py
+++ dia5/backend/app/blueprints/shop/resources/producto.py
@@ -31,5 +31,63 @@ class ProductoResource(Resource):
         
         return context
     
+    def get(self):
+        data = Producto.get_all()
+        data_schema = ProductoSchema(many=True)
+        context = {
+            'status':True,
+            'content':data_schema.dump(data)
+        }
+        
+        return context
+    
+class ProductoDetailResource(Resource):
+    
+    def get(self,id):
+        producto = Producto.get_by_id(id)
+        data_schema = ProductoSchema()
+        context = {
+            'status':True,
+            'content':data_schema.dump(producto)
+        }
+        
+        return context
+    
+    def put(self,id):
+        data = request.get_json()
+        nombre = data['nombre']
+        precio = data['precio']
+        descripcion = data['descripcion']
+        imagen = data['imagen']
+        
+        producto = Producto.get_by_id(id)
+        producto.nombre = nombre
+        producto.precio = precio
+        producto.descripcion = descripcion
+        producto.imagen = imagen
+        producto.save()
+        
+        data_schema = ProductoSchema()
+        
+        context = {
+            'status':True,
+            'content':data_schema.dump(producto)
+        }
+        
+        return context
+    
+    def delete(self,id):
+        producto = Producto.get_by_id(id)
+        producto.delete()
+        
+        data_schema = ProductoSchema()
+        
+        context = {
+            'status':True,
+            'content':data_schema.dump(producto)
+        }
+        
+        return context   
     
 api.add_resource(ProductoResource,'/producto')
+api.add_resource(ProductoDetailResource,'/producto/<id>')
