c9b3aec90f3b7fa602f7c3a4485d6e3f4fab3643
César Mayta <cesarmayta@gmail.com>
2023-10-13 22:10:27 -0500

    producto models

--- dia5/backend/app/blueprints/shop/models.py
+++ dia5/backend/app/blueprints/shop/models.py
@@ -51,4 +51,39 @@ class Marca(db.Model):
         
     def delete(self):
         db.session.delete(self)
-        db.session.commit()     
+        db.session.commit()
+        
+class Producto(db.Model):
+    __tablename__ = 'tbl_producto'
+    
+    id = db.Column(db.Integer,primary_key=True)
+    nombre = db.Column(db.String(200),nullable=False)
+    descripcion = db.Column(db.Text,nullable=True)
+    precio = db.Column(db.Double,default=0)
+    imagen = db.Column(db.String(255))
+    categoria_id = db.Column(db.Integer,db.ForeignKey("tbl_categoria.id"))
+    marca_id = db.Column(db.Integer,db.ForeignKey("tbl_marca.id"))
+    
+    def __init__(self,nombre,precio,imagen,categoria_id,marca_id):
+        self.nombre = nombre
+        self.precio = precio
+        self.imagen = imagen
+        self.categoria_id = categoria_id
+        self.marca_id = marca_id
+        
+    @staticmethod
+    def get_all():
+        return Producto.query.all()
+    
+    @staticmethod
+    def get_by_id(id):
+        return Producto.query.get(id)
+    
+    def save(self):
+        if not self.id:
+            db.session.add(self)
+        db.session.commit()
+        
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
