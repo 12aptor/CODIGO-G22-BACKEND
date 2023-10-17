d67ee15b42a3a7453aeb78b3fa260e1e7eae3bc7
César Mayta <cesarmayta@gmail.com>
2023-10-16 20:05:11 -0500

    dia1

--- dia4/backend/app/blueprints/shop/__init__.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-shop = Blueprint('shop',__name__,url_prefix='/')
-
-from .models import Categoria
-
-from .resources import (
-    index
-)
--- dia5/backend/app/config.py
+++
@@ -1,3 +0,0 @@
-class Config:
-    SQLALCHEMY_DATABASE_URI='mysql://root:root@localhost/db_shop_flask'
-    SQLALCHEMY_TRACK_MODIFICATIONS=False
