73843db58858b62c99f000bfa23eb671ad0a79f3
César Mayta <cesarmayta@gmail.com>
2023-10-17 20:27:33 -0500

    jwt manager

--- dia2/ecommerce/backend/app/config.py
+++ dia2/ecommerce/backend/app/config.py
@@ -1,3 +1,7 @@
+from datetime import timedelta
+
 class Config:
     SQLALCHEMY_DATABASE_URI='mysql://root:root@localhost/db_shop_flask'
     SQLALCHEMY_TRACK_MODIFICATIONS=False
+    JWT_SECRET_KEY='ES8ea1Zs01tBcDGPJxYzhoXfEES8ea1Zs01tBcDGPJxYzhoXfE'
+    JWT_ACCESS_TOKEN_EXPIRES=timedelta(hours=1)
