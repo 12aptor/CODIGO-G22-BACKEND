95e273166a717ffaf84df5fc750849564bc44289
César Mayta <cesarmayta@gmail.com>
2023-10-19 21:52:31 -0500

    flask base datos

--- dia4/repaso/proyecto-base/app/__init__.py
+++ dia4/repaso/proyecto-base/app/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask,jsonify
+from .config import Config
 
 from .api import api
 
 def create_app():
     app = Flask(__name__)
-    
+    app.config.from_object(Config)
     app.register_blueprint(api)
     
     @app.route('/')
---
+++ dia4/repaso/proyecto-base/app/config.py
@@ -0,0 +1,3 @@
+class Config:
+    SQLALCHEMY_DATABASE_URI='mysql://root:root@localhost/db_proyecto_base'
+    SQLALCHEMY_TRACK_MODIFICATIONS=False
