b2723a0b7c752f86ed89743ae44056ee09a833ff
César Mayta <cesarmayta@gmail.com>
2023-10-24 22:58:25 -0500

    dia2 orm final

--- dia2/orm/proyecto/recetas/admin.py
+++ dia2/orm/proyecto/recetas/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Autor,Receta,Comentario
+
+admin.site.register(Autor)
+admin.site.register(Receta)
+admin.site.register(Comentario)
