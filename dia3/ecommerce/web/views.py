6633bc215b866622c68e75b49e941665534b1dea
Cesar Edteam <cesaredteam@gmail.com>
2023-10-25 22:49:04 -0500

    dia3 catalogo inicial listo

--- dia3/ecommerce/web/views.py
+++ dia3/ecommerce/web/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render
 
+from .models import Categoria,Marca,Producto
+
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    lista_categorias = Categoria.objects.all()
+    lista_marcas = Marca.objects.all()
+    lista_productos = Producto.objects.all()
+    
+    context = {
+        'categorias':lista_categorias,
+        'marcas':lista_marcas,
+        'productos':lista_productos
+    }
+    
+    return render(request,'index.html',context)
