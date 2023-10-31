de19a4f027f38c457e43729cbd9bae7a4a9dd873
Cesar Edteam <cesaredteam@gmail.com>
2023-10-30 22:51:06 -0500

    dia1 tarea get

--- dia1/backend/api/models.py
+++ dia1/backend/api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Tarea(models.Model):
+    ESTADO_CHOICES = (
+        ('pendiente','pendiente'),
+        ('completado','completado')
+    )
+    
+    descripcion = models.CharField(max_length=200)
+    estado = models.CharField(max_length=100,choices=ESTADO_CHOICES)
+    
+    def __str__(self):
+        return self.descripcion
--- dia1/backend/api/views.py
+++ dia1/backend/api/views.py
@@ -1,11 +1,27 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 
+from .models import Tarea
+from .serializers import TareaSerializer
+
 @api_view(['GET'])
 def index(request):
     context = {
         'status':True,
         'message':'mi primer api rest con django rest framework'
+    }
+    
+    return Response(context)
+
+@api_view(['GET','POST'])
+def tarea(request):
+    if request.method == 'GET':
+        data = Tarea.objects.all() #queryset
+        serializer = TareaSerializer(data,many=True)
+        
+    context = {
+        'status':True,
+        'content':serializer.data
     }
     
     return Response(context)
