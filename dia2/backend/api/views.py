af81d042f9c2375fcddb3f93ec36b685e76cb8f1
Cesar Edteam <cesaredteam@gmail.com>
2023-10-31 20:00:06 -0500

    dia2 indexview

--- dia2/backend/api/views.py
+++ dia2/backend/api/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class IndexView(APIView):
+    
+    def get(self,request):
+        context = {
+            'status':True,
+            'content':'api rest activo'
+        }
+        return Response(context)
