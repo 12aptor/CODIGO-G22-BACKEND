7126575d190d25fe6922fe8e484f886358da671c
Cesar Edteam <cesaredteam@gmail.com>
2023-11-07 19:58:56 -0500

    dia5 swagger listo

--- dia5/devhub/backend/devhub/urls.py
+++ dia5/devhub/backend/devhub/urls.py
@@ -28,11 +28,11 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="Snippets API",
+      title="API REST DEVHUB",
       default_version='v1',
-      description="Test description",
+      description="API Rest de portal de ofertas laborales",
       terms_of_service="https://www.google.com/policies/terms/",
-      contact=openapi.Contact(email="contact@snippets.local"),
+      contact=openapi.Contact(email="cesarmayta@gmail.com"),
       license=openapi.License(name="BSD License"),
    ),
    public=True,
@@ -41,7 +41,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/',include('api.urls')),
     path('admin/', admin.site.urls),
