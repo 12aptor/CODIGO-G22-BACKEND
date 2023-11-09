e8d8c9c013009559d38825137941857765893feb
Cesar Edteam <cesaredteam@gmail.com>
2023-11-08 21:57:44 -0500

    dia2 parametros

--- dia2/index.js
+++ dia2/index.js
@@ -6,4 +6,16 @@ app.get('/',(req,res)=>{
     res.send('<h1><center>Mi primer servidor con express</center></h1>')
 })
 
+app.get('/json',(req,res)=>{
+    res.json({
+        'status':true,
+        'content':'primer api rest con express'
+    })
+})
+
+app.get('/saludo',(req,res)=>{
+    let nombre = req.query.nombre
+    res.send('<h1>Hola ' + nombre + '</h1>')
+})
+
 app.listen(5000,()=>console.log('servidor en http://localhost:5000'))
