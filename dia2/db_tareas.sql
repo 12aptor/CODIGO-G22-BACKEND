8434d4004271c5fd80d9ce0708da7dfaa1f146cf
Cesar Edteam <cesaredteam@gmail.com>
2023-11-08 22:24:03 -0500

    dia2 db_tareas

---
+++ dia2/db_tareas.sql
@@ -0,0 +1,11 @@
+
+DROP TABLE IF EXISTS `tarea`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tarea` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `descripcion` varchar(200) NOT NULL,
+  `estado` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4;
+/*!40101 SET character_set_client = @saved_cs_client */;
