e75864e1f75114a6bd040365ef1ce955d53d15d7
Cesar Edteam <cesaredteam@gmail.com>
2023-11-08 22:15:55 -0500

    dia2 nodemon

--- dia2/index.js
+++ dia2/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send('<h1><center>Mi primer servidor con express</center></h1>')
+    res.send('<h1><center>Mi primer servidor con express.js</center></h1>')
 })
 
 app.get('/json',(req,res)=>{
@@ -16,6 +16,12 @@ app.get('/json',(req,res)=>{
 app.get('/saludo',(req,res)=>{
     let nombre = req.query.nombre
     res.send('<h1>Hola ' + nombre + '</h1>')
+})
+
+app.get('/suma/:n1/:n2',(req,res)=>{
+    const {n1,n2} = req.params
+    let suma = parseInt(n1) + parseInt(n2)
+    res.send(`<h1>la suma de ${n1} + ${n2}  es ${suma}</h1>`)
 })
 
 app.listen(5000,()=>console.log('servidor en http://localhost:5000'))
