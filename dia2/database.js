7c05dc158fe9906df80abc8c576917745d9cb632
Cesar Edteam <cesaredteam@gmail.com>
2023-11-08 22:34:36 -0500

    database.js

---
+++ dia2/database.js
@@ -0,0 +1,18 @@
+const mysql2 = require('mysql2')
+
+const mysqlConnection = mysql2.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'db_tareas_g22',
+    port:'3306'
+})
+
+mysqlConnection.connect(function(err){
+    if(err){
+        console.error(err)
+        
+    }else{
+        console.log('conectado a bd')
+    }
+})
