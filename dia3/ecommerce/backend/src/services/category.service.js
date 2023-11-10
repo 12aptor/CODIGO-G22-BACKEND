07de3ea744ff138a604cb545736fcad94247e353
Cesar Edteam <cesaredteam@gmail.com>
2023-11-09 22:28:24 -0500

    update

--- dia3/ecommerce/backend/src/services/category.service.js
+++ dia3/ecommerce/backend/src/services/category.service.js
@@ -28,7 +28,18 @@ class CategoryService{
         const result = await this.db.querySql(query)
         return result
     }
-    
+
+    async update({data,id}){
+        const queryUpdate = `update tbl_category set
+                             name = '${data.name}'
+                             where id = ${id}`
+        await this.db.querySql(queryUpdate)
+        const queryUpdated = `select id,name from tbl_category
+                              where id = ${id}`
+        const result = await this.db.querySql(queryUpdated)
+        return result
+    }
+
 }
 
 module.exports = CategoryService
