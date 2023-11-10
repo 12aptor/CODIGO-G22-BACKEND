56001063b5483e64ca9e02ed944afc81da295cb6
Cesar Edteam <cesaredteam@gmail.com>
2023-11-09 20:59:07 -0500

    dia3 index.js con routes categories

--- dia3/ecommerce/backend/src/index.js
+++ dia3/ecommerce/backend/src/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const {config} = require('./config')
 
+const categoryApi = require('./routes/category.routes')
+
 const app = express()
 
 app.get('/',(req,res)=>{
@@ -9,5 +11,7 @@ app.get('/',(req,res)=>{
         'message':'api rest version 1.0'
     })
 })
+
+categoryApi(app)
 
 app.listen(config.port,()=>console.log('http://localhost:'+config.port))
