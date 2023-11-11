3f005c9eb9a18752c39f92ffc2d8d5a63040c692
Cesar Edteam <cesaredteam@gmail.com>
2023-11-10 19:42:02 -0500

    dia4 middlewares de ruta

--- dia4/middlewares/index.js
+++ dia4/middlewares/index.js
@@ -3,19 +3,36 @@ const express = require('express')
 const app = express()
 
 /************ MIDDLEWARES ***********/
+
+//MIDDLEWARES DE APLICACIÓN
 app.use(function(req,res,next){
     console.log("eso es un middleware")
     console.log("tipo de request : ",req.method)
     next()
 })
 
-
-/************* RUTAS  ***************/
 app.get('/',(req,res)=>{
     res.json({
         status:true,
         content:'ejemplo de middlewares'
     })
 })
+
+//MIDDLEWARES DE RUTA
+app.use('/usuario',(req,res,next)=>{
+    const timeElapsed = Date.now()
+    const today = new Date(timeElapsed)
+    console.log('ejecutado a las ',today.toISOString())
+    next()
+})
+
+app.get('/usuario',(req,res)=>{
+    res.json({
+        nombre:'admin',
+        email:'admin@gmail.com'
+    })
+})
+
+
 
 app.listen(5000,()=>console.log('http://localhost:5000'))
