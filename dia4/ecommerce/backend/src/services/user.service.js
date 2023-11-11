f9f0a7aa0831f1d281a3fe89025094fe550c8ad5
Cesar Edteam <cesaredteam@gmail.com>
2023-11-10 22:39:32 -0500

    user service authenticate

--- dia4/ecommerce/backend/src/services/user.service.js
+++ dia4/ecommerce/backend/src/services/user.service.js
@@ -23,6 +23,30 @@ class UserService{
         const result = await this.getLast()
         return result
     }
+
+    async authenticate({data}){
+        const userNotFound = {
+            id:0,
+            username:null
+        }
+        try{
+            const query = `select id,username,password
+                           from tbl_user where username = ${data.username}`
+
+            const result = await this.db.querySql(query)
+            if(await bcrypt.compare(data.password,result[0].password)){
+                const userFound = {
+                    id:result[0].id,
+                    username:data.username
+                }
+                return userFound
+            }else{
+               return userNotFound
+            }
+        }catch(err){
+              return userNotFound
+        }
+    }
 }
 
 module.exports = UserService
