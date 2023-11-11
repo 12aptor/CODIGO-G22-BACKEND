0032bdc7c04b6bb1c4faa0f888a1947c5318f145
Cesar Edteam <cesaredteam@gmail.com>
2023-11-10 20:46:02 -0500

    boom

--- dia4/ecommerce/backend/src/index.js
+++ dia4/ecommerce/backend/src/index.js
@@ -1,18 +1,30 @@
 const express = require('express')
 const {config} = require('./config')
+const boom = require('@hapi/boom')
 
 const categoryApi = require('./routes/category.routes')
+
+//middlewares
+const {errorHandler,boomErrorHandler} = require('./middlewares/error.handler')
 
 const app = express()
 app.use(express.json())
 
 app.get('/',(req,res)=>{
-    res.json({
-        'status':true,
-        'message':'api rest version 1.0'
-    })
+    try{
+        console.log(a + 3)
+        res.json({
+            'status':true,
+            'message':'api rest version 1.0'
+        })
+    }catch(err){
+        res.status(500).json(boom.badData('error : ' + err.message))
+    }
 })
 
 categoryApi(app)
+
+app.use(boomErrorHandler)
+app.use(errorHandler)
 
 app.listen(config.port,()=>console.log('http://localhost:'+config.port))
