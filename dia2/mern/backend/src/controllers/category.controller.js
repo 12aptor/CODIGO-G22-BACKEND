df67c05c009ace5856b4987ca5b45153923c97ab
Cesar Edteam <cesaredteam@gmail.com>
2023-11-16 20:34:13 -0500

    controllers

---
+++ dia2/mern/backend/src/controllers/category.controller.js
@@ -0,0 +1,18 @@
+const categoryController = {}
+
+const CategoryModel = require('../models/category.model')
+
+categoryController.create = async (req,res)=>{
+    try{
+        const newCategory = new CategoryModel(req.body)
+        await newCategory.save()
+        res.status(201).json({
+            status:true,
+            content:newCategory
+        })
+    }catch(err){
+        res.status(502).json({
+            message:err
+        })
+    }
+}
