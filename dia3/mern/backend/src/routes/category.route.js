e26706a31117d5401866bce98e406f6fc328c20e
Cesar Edteam <cesaredteam@gmail.com>
2023-11-17 22:41:18 -0500

    verify token in categories

--- dia3/mern/backend/src/routes/category.route.js
+++ dia3/mern/backend/src/routes/category.route.js
@@ -1,15 +1,17 @@
 const {Router} = require('express')
 const router = Router()
 
+const {verifyToken} = require('../middlewares/auth.handler')
+
 const {create,getAll,getOne,updateOne,deleteOne} = require('../controllers/category.controller')
 
 router.route('/')
-.post(create)
+.post(verifyToken,create)
 .get(getAll)
 
 router.route('/:id')
-.get(getOne)
-.put(updateOne)
-.delete(deleteOne)
+.get(verifyToken,getOne)
+.put(verifyToken,updateOne)
+.delete(verifyToken,deleteOne)
 
 module.exports = router
