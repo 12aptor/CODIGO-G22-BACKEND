d8d4e88b3b61a2fb1a7d58c70c3cee50c0d914c3
Cesar Edteam <cesaredteam@gmail.com>
2023-11-20 22:57:47 -0500

    pm2 start ecosystem

---
+++ dia4/microservicios/backend/ecosystem.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  apps : [{
+    name   : "ms-courses",
+    script : "./src/microservices/ms-courses/index.js"
+  },
+  {
+    name   : "ms-auth",
+    script : "./src/microservices/ms-auth/index.js"
+  }]
+}
